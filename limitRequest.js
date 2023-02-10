f55f776845089502f07704582d390b900ff9a2ac
“zhangzheng” <1562124117@qq.com>
2023-02-10 10:08:29 +0800

    并发请求

--- limitRequest.js
+++ limitRequest.js
@@ -30,7 +30,7 @@ class Scheduler {
             this.request()
         })
     }
-    async limitRequest () {
+    async limitRequest() {
         let pool = []
         if (this.limit >= this.queue.length) {
             Promise.all(this.queue)
@@ -79,7 +79,7 @@ async function demo() {
             console.log(data);
             //请求结束后将该Promise任务从并发池中移除
             let index = pool.findIndex(t => t === task)
-            pool.splice(index,1)
+            pool.splice(index, 1)
         })
         pool.push(task)
         if (pool.length === max) {
@@ -93,3 +93,81 @@ async function demo() {
     console.log("结束了");
 }
 // demo()
+const pLimit = (concurrency) => {
+    if (!((Number.isInteger(concurrency) || concurrency === Infinity) && concurrency > 0)) {
+        throw new TypeError('Expected `concurrency` to be a number from 1 and up');
+    }
+    const queue = [];
+    let activeCount = 0;
+    const next = () => {
+        activeCount--;
+        if (queue.length > 0) {
+            queue.shift()();
+        }
+    };
+    const run = async (fn, resolve, ...args) => {
+        activeCount++;
+        const result = (async () => fn(...args))();
+        resolve(result);
+        try {
+            await result;
+        } catch {}
+        next();
+    };
+    // 因为 activeCount-- 的逻辑是在执行完任务之后才执行的，万一任务还没执行完，这时候 activeCount 就是不准的。
+    // 所以为了保证并发数量能控制准确，要等全部的微任务执行完再拿 activeCount。
+    // 怎么在全部的微任务执行完再执行逻辑呢？
+    // 加一个新的微任务不就行了？
+    // 所以有这样的 await Promise.resolve(); 的逻辑
+    const enqueue = (fn, resolve, ...args) => {
+        queue.push(run.bind(null, fn, resolve, ...args));
+        (async () => {
+            await Promise.resolve();
+
+            if (activeCount < concurrency && queue.length > 0) {
+                queue.shift()();
+            }
+        })();
+    };
+    const generator = (fn, ...args) =>
+        new Promise((resolve) => {
+            enqueue(fn, resolve, ...args);
+        });
+
+    Object.defineProperties(generator, {
+        activeCount: {
+            get: () => activeCount
+        },
+        pendingCount: {
+            get: () => queue.length
+        },
+        clearQueue: {
+            value: () => {
+                queue.length = 0;
+            }
+        }
+    });
+
+    return generator;
+};
+const limit = pLimit(2);
+
+function asyncFun(value, delay) {
+    return new Promise((resolve) => {
+        console.log('start ' + value);
+        setTimeout(() => resolve(value), delay);
+    });
+}
+
+(async function () {
+    const arr = [
+        limit(() => asyncFun('aaa', 2000)),
+        limit(() => asyncFun('bbb', 3000)),
+        limit(() => asyncFun('ccc', 1000)),
+        limit(() => asyncFun('ccc', 1000)),
+        limit(() => asyncFun('ccc', 1000))
+    ];
+
+    const result = await Promise.all(arr);
+    console.log(result);
+})();
