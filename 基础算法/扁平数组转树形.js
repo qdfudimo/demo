3240e101380a8c6ce0be648c2515b64e09b9c517
“zhangzheng” <1562124117@qq.com>
2023-03-23 10:19:24 +0800

    fix:树形方法

--- 基础算法/扁平数组转树形.js
+++ 基础算法/扁平数组转树形.js
@@ -9,6 +9,47 @@ var data = [
     { id: 6, pid: 13, name: '卫生纸' },
     { id: 7, pid: 3, name: '薯片' },
 ];
+//时间复杂度：O(n^2)，其中 n 是节点的数量。 空间复杂度：O(n^2)。 优缺点：不适合大规模数据。
+function arrayToTreeRec(nodes, pid = null) {
+    return nodes
+      .filter((node) => node.pid === pid)
+      .map((node) => ({ ...node, children: arrayToTreeRec(nodes, node.id) }));
+  }
+console.log(arrayToTreeRec(data, 0))
+function arrayToTreeReduce(nodes,pid = 0) {
+    const map = {};
+    const tree = nodes.reduce((acc, node) => {
+      map[node.id] = { ...node, children:(map[node.id] && map[node.id].children)|| [] };
+      if (node.pid === pid) {
+        acc.push(map[node.id]);
+      } else {
+        if (!map[node.pid]) {
+            map[node.pid] = {
+                children: []
+            }
+        }
+        map[node.pid].children.push(map[node.id]);
+      }
+      return acc;
+    }, []);
+  
+    return tree;
+  }
+  function arrayToTreeMap(nodes) {
+    const map = new Map(nodes.map((node) => [node.id, { ...node, children: [] }]));
+    const tree = [];
+  
+    for (const node of map.values()) {
+      if (node.parentId === null) {
+        tree.push(node);
+      } else {
+        map.get(node.parentId).children.push(node);
+      }
+    }
+  
+    return tree;
+  }
+  
 //根据对象的引用关系
 function recurrenceFilter(items) {
     const result = [];   // 存放结果集
@@ -32,6 +73,7 @@ function recurrenceFilter(items) {
     return result;
 }
 console.log(recurrenceFilter(data));
+//有bug 数据混乱的情况下 还是会缺少
 function recurrenceFilter(data) {
     var result = [];//存储结果
     var map = new Map(); //存在id,对应所在的内存地址
