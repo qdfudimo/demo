3042394e9d96abe85646cc96228d68c706d8877d
zhangzheng <1562124117@qq.com>
2023-06-19 11:10:48 +0800

    fix：增加权限测试

--- vue-pinia/src/App.vue
+++ vue-pinia/src/App.vue
@@ -1,84 +1,37 @@
 <script setup>
-import { reactive ,ref} from "vue";
+import { reactive, ref, watchEffect, h } from "vue";
 import HelloWorld from './components/HelloWorld.vue';
-
-const handelClick = () => {
+import testPage from "./components/test-page.vue";
+import vAuth from "./directive/permission"
+const handelClick = (flag) => {
   // codeList.push(4)
+  if (flag) {
+    curCode.value = 3
+    return
+  }
   curCode.value = 4
 }
-const codeList = reactive([1, 2, 3])
 const curCode = ref(3)
-const removeEl = (el) => {
-  // 在绑定元素上存储父级元素
-  el._parentNode = el.parentNode
-  // 在绑定元素上存储一个注释节点
-  el._placeholderNode = document.createComment("auth")
-  // 使用注释节点来占位
-  el.parentNode?.replaceChild(el._placeholderNode, el)
-}
-
-const addEl = (el) => {
-  // 替换掉给自己占位的注释节点
-  el._parentNode?.replaceChild(el, el._placeholderNode)
-}
-
-const hasPermission = (value) => {
-  return codeList.includes(value)
-}
-const vAuth = {
-  mounted: (el, binding) => {
-    const value = binding.value
-    if (!value) return
-    if (!hasPermission(value)) {
-      // 挂载的时候没有权限把元素删除
-      removeEl(el)
-    }
-  },
-  updated(el, binding) {
-    console.dir(el);
-    console.log("更新了");
-    let update = () => {
-      let valueNotChange = binding.value == binding.oldValue
-      let oldHasPermission = hasPermission(binding.oldValue)
-      let newHasPermission = hasPermission(binding.value)
-      let permissionNotChange = oldHasPermission === newHasPermission
-      if (valueNotChange && permissionNotChange) return
-      if (newHasPermission) {
-        addEl(el)
-      } else {
-        removeEl(el)
-      }
-    };
-    if (el._watchEffect) {
-      update()
-    } else {
-      el._watchEffect = watchEffect(() => {
-        update()
-      })
-    }
-  },
-  beforeUnmount(el, binding, vnode, prevVnode) {
-    console.log("卸载了");
-  },
-}
-
+const headerRender = ({ column }) => h("div", null, `${column.label}(渲染函数自定义表头)`)
 </script>
 
 <template>
+  <component :is="headerRender" :column="{ 'label': '自定义' }" />
+  <test-page>
+    <div>2222</div>
+    <div>22200000</div>
+  </test-page>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
     <button hover-class="button-hover" v-auth="curCode">
       增加数据权限测试
     </button>
   </div>
   <HelloWorld msg="Vite + Vue" />
-  <button hover-class="button-hover" @click="handelClick">
+  <button hover-class="button-hover" @click="() => handelClick(false)">
     点击更新
+  </button>
+  <button hover-class="button-hover" @click="() => handelClick(true)">
+    再次点击更新
   </button>
 </template>
 
--- vue-pinia/src/main.js
+++ vue-pinia/src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { createPinia } from "./pinia";
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 const app = createApp(App)
-app.use(createPinia()).mount('#app')
+app.use(ElementPlus)
+app.use(createPinia())
+const el =app.mount('#app')
+console.log(el);
