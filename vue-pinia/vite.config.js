e940bb0765a4009d97d32288fd610229773d9995
zhangzheng <1562124117@qq.com>
2023-12-22 10:48:31 +0800

    copy vite复制文件

--- vue-pinia/vite.config.js
+++ vue-pinia/vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
+import {
+  defineConfig
+} from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import copy from 'rollup-plugin-copy'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  publicDir: false,
+  // build: {
+  //   copyPublicDir: false
+  // },
+  plugins: [vue(),
+    copy({
+      targets: [{
+        src: 'public/demo/demo.js',
+        dest: 'dist/demo123',
+        rename: (name, extension, fullPath) => {
+          console.log(name, extension, 8888888888888888888888888888888);
+          return `${name}-${+new Date()}.${extension}`
+        }
+      } ],
+      hook: 'writeBundle',
+      // hook: 'writeBundle',
+    })
+  ],
+
 })
