52457ef5ba5ebc485ee9719c00253cb003b91aef
zhangzheng <1562124117@qq.com>
2024-05-13 19:23:33 +0800

    feat：请求缓存

--- axios请求并发控制.js
+++ axios请求并发控制.js
@@ -68,24 +68,24 @@ import axios from 'axios';
 import ConcurrentInterceptors from './concurrentInterceptors';
 // 创建并发实例对象,并设置最大并发数量:2
 const concurrent = new ConcurrentInterceptors(2)
- 
+
 // 创建axios实例对象
 const instance = axios.create({});
- 
+
 // axios自身的请求拦截器
 instance.interceptors.request.use(
     async config => {
-        /**
-         * 关键代码 
-         * @description 并发拦截器启动 进行数量检测
-         */
-        await concurrent.interceptors()
- 
-        return config;
-    },
-    error => Promise.reject(error)
+            /**
+             * 关键代码 
+             * @description 并发拦截器启动 进行数量检测
+             */
+            await concurrent.interceptors()
+
+            return config;
+        },
+        error => Promise.reject(error)
 );
- 
+
 // axios自身的响应拦截器
 instance.interceptors.response.use(
     response => {
@@ -94,7 +94,7 @@ instance.interceptors.response.use(
          * @description 通知并发拦截器有任务完成了 请放行下一个任务!
          */
         concurrent.next()
- 
+
         return response;
     },
     error => {
@@ -103,7 +103,67 @@ instance.interceptors.response.use(
          * @description 通知并发拦截器有任务完成了 请放行下一个任务!
          */
         concurrent.next()
-        
+
         return Promise.reject(error);
     }
 );
+/**
+ * 同时请求多次之请求第一次，别的以第一次结果为准
+ */
+let cacheResult = new Map()
+
+function cacheRequest(methods, url, params) {
+    //生成参数加url的唯一值
+    let str = getUnicode(params, url);
+    let current = cacheResult.get(str)
+    if (current) {
+        return new Promise((resolve, reject) => {
+            if (current.type == "pendding") {
+                current.resolves.push(resolve)
+                current.rejects.push(reject)
+            } else if (current.type == "resolve") {
+                //为了防止元数据污染
+                resolve(cloneDeep(current.data))
+            } else {
+                reject(cloneDeep(current.data))
+            }
+        })
+    } else {
+        current = {
+            type: "pendding",
+            resolves: [],
+            rejects: []
+        }
+        cacheResult.set(str, current);
+        let args = [url];
+        methods = methods.toLowerCase()
+        if (methods == "post" && params) {
+            args.push(params)
+        }
+        return axios[methods](url, params).then(res => {
+            if (current.resolves && current.resolves.length) {
+                current.resolves.forEach(item => {
+                    item(cloneDeep(e))
+                });
+                current.resolves = null
+                current.rejects = null
+            }
+            current.type = "resolve"
+            current.data = cloneDeep(e)
+            cacheResult.set(str, current);
+            return Promise.resolve(cloneDeep(e))
+        }).catch(e => {
+            if (current.rejects && current.rejects.length) {
+                current.rejects.forEach(item => {
+                    item(cloneDeep(e))
+                });
+                current.resolves = null
+                current.rejects = null
+            }
+            current.type = "reject"
+            current.data = cloneDeep(e)
+            cacheResult.set(str, current);
+            return Promise.reject(cloneDeep(e))
+        })
+    }
+}
