3b2cd637263dc007e34406e9cad0549c670785c7
zhangzheng <1562124117@qq.com>
2026-01-24 13:06:58 +0800

    feat(fileUpload): 添加文件上传功能页面
    新增文件上传页面，支持多文件选择和上传功能，包含基本的HTML结构、
    CSS样式和JavaScript逻辑，使用FormData处理文件上传请求。

--- vue-pinia/vite.config.js
+++ vue-pinia/vite.config.js
@@ -3,6 +3,9 @@ import {
 } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import copy from 'rollup-plugin-copy'
+//可以预览 编译的过程
+import Inspect from 'vite-plugin-inspect'
+import VueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   publicDir: false,
@@ -10,6 +13,11 @@ export default defineConfig({
   //   copyPublicDir: false
   // },
   plugins: [vue(),
+    VueDevTools(), // 添加到这里
+    Inspect({
+      build: true,
+      opne:true//自动打开 /__insepect/页面
+    }),
     // VitePluginStyleInject()
     copy({
       targets: [{
